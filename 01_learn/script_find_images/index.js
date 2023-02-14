ddadc2e92ae30c135ce8bd9d0d9d8a25bdc457b0
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2023-02-14 12:02:48 -0600

    script find images added

---
+++ 01_learn/script_find_images/index.js
@@ -0,0 +1,20 @@
+const shelljs = require("shelljs");
+const fs = require("fs");
+
+const imagesFolderpath = "/Users/oscar.vasquez/Documents/03_web/2023/02_Chris_Juego_GIT/AAV-MythBusters-Game/angular/src/assets";
+
+const paths = shelljs
+  .find(imagesFolderpath)
+  .filter(function (file) {
+    return file.match(/[\.png$|\.jpg$]/);
+  })
+  .filter((str) => str.toString().endsWith(".jpg") || str.toString().endsWith(".png"))
+  .map((str) => {
+    const pathToReplace = imagesFolderpath.replace("assets", "");
+    return `'${str.replace(pathToReplace, "/")}'`;
+  });
+
+fs.writeFile("paths.txt", paths.toString(), (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
